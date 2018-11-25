1d5a771e943e0bbc635ab051d07d3dd8a65ee838
mr.kalinuk <qazwsx159357>
2018-11-25 15:17:46 +0200

    Your Message

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ app.use(express.static(__dirname + '/publick/'));
 app.use(cookieParser());
 app.use(bParser.raw({limit: '50mb'}));
 
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
 //routes pages
 const index = require('./routes/getIndex');
 const tovar = require('./routes/getTovar');
