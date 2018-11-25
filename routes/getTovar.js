8944d52dc4214a6fd4a334e9ed7be039afd43f22
mr.kalinuk <qazwsx159357>
2018-11-25 15:18:59 +0200

    Your Message

--- routes/getTovar.js
+++ routes/getTovar.js
@@ -68,6 +68,15 @@ router.get('/*', function(req, res, next){
                       console.log(results_tovar.length)
                       var paginator = new pagination.SearchPaginator({prelink: '/shop?c='+searchData[0]+','+searchData[1].split('&')[0], current: current_page, rowsPerPage: 24, totalResult: results_tovar.length});
                       var p = paginator.getPaginationData();
+                      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+                      // Request methods you wish to allow
+                      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+                      // Request headers you wish to allow
+                      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+                      // Set to true if you need the website to include cookies in the requests sent
+                      // to the API (e.g. in case you use sessions)
+                      res.setHeader('Access-Control-Allow-Credentials', true);
+                      
                       res.render('tovar.ejs',{
                         conf: results_config[languageSystem],
                         menu: results_menu,
