99aba27eedc4454f5fdedb41de9be250a4929075
mr.kalinuk <qazwsx159357>
2018-11-28 21:55:54 +0200

    Your Message

--- publick/script/tovar/tovar.js
+++ publick/script/tovar/tovar.js
@@ -97,7 +97,7 @@ var Index = {
          $(".basket_tovar .BSK").append(newDiv)
          summa = summa + parseFloat(tovar.tovar[i].price);
          var minBasImg = document.createElement("div");
-         minBasImg.style.backgroundImage = "url(../../../data/tovar/"+tovar.tovar[i].AI + '/' +tovar.tovar[i].image[0]+")";
+         minBasImg.style.backgroundImage = "url(../../../data/tovar/" + tovar.tovar[i].image[0]+")";
          minBasImg.className = "minBasImg";
 
          var minBasTitle = document.createElement("div");
